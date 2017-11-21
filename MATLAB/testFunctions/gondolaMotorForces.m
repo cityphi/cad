8b3f027d1cac16dc2a3ad2a6c52b58d2c72b6189
Isaak Goldenberg <isaak.goldenberg@gmail.com>
2017-11-20 22:30:48 -0500

    much good
    much good has been done to code

--- MATLAB/testFunctions/gondolaMotorForces.m
+++ MATLAB/testFunctions/gondolaMotorForces.m
@@ -1,54 +1,72 @@
 %Forces is [locX locY locZ Fx Fy Fz Mx My Mz] 
 %Test for forces acting on gondola driving motor
+%all lengths/distances are in [m] other units are specified
 
-Tw = 0.1;             %motor torque
-Ls = 0.04;             %distance between screws
-Lhs = 0.05;            %length from motor hinge axle to motor mount screws
-Lsw = 0.015;            %length from motor mount screw to friction wheel contact point
-La = 0.01;             %length from motor hinge axle to gondola screw a 
-Lb = 0.03;             %length from motor hinge axle to gondola screw b 
-Hsw = 0.024;            %height of friction wheel contact point off hinge
+Tw = 0.1;           %motor torque [Nm]
+
+Ls = 0.08;          %distance from screw to center of torsion hinge 
+Lhd = 0.02185;      %distance in y from torsion hingle to friction wheel contact point 
+Hdrive = 0.021125;  %height of friction wheel contact point above face of gond
+Ldrivex = 0.05;     %distance in x from drive to gondola axle
+Ldrivey = 0.00362;  %distance in y from drive to center of gondola
 rFw = 0.0127;       %friction wheel radius
+
+La = 0.005;         %length in y from torsion hinge to gondola screw a 
+Lb = 0.021;         %length in y from torsion hinge to gondola screw b
+Dscrew = 0.003;     %gondola/hinge screw diameter 
+Dwashero = 0.005;   %outer gondola/hinge screw washer diameter
+Dwasheri = 0.003;   %inner gondola/hinge screw washer diameter
+
 Mu = 0.65;          %frictionwheel to keel coefficient of friction
 Muwasher = 0.2;     %washer to gondola coefficent of friction
-Dscrew = 0.003;     %gondola/hinge screw diameter 
-Dwashero = 0.005;   %outer washer diameter
-Dwasheri = 0.003;   %inner washer diameter
+
 Scompressive = 55;  %The compressive yield strength of Nylon 6 [Mpa]
-Lgond = 0.15        %length of one gondola car
-Wgond = 0.075       %width of gondola 
+g = 9.81;           %acceleration due to gravity [m/s^2]
 
-Tspring = 1.5 * (Tw * (Lhs+Lsw))/(rFw * Mu); %spring torque
+Lbearingx = 0.03682;%distance in x from bearing contact to gondola axle
+Lbearingy = 0.00328;%distance in y from bearing contact to center of gondola
+Hbearing = 0.03954; %height in z from bearing contact to surface of gondola
 
-Fspring =  Tspring /(Lhs); %force of spring acting on friction wheel
+Lgond = 0.113515;   %length in x of one gondola car
+Wgond = 0.06;       %width in y of gondola 
+Hgond = 0.06;       %height in z of gondola
+Lcm1x = -0.055;     %center of gravity in x or gondola 1
+Lcm1y = 0;          %center of gravity in x or gondola 1
+Lcm1z = -0.030;     %center of gravity in x or gondola 1
+Lcm2x = 0.055;      %center of gravity in x or gondola 2
+Lcm2y = 0;          %center of gravity in x or gondola 2
+Lcm2z = -0.03;      %center of gravity in x or gondola 2
+
+Tspring = 1.5 * (Tw * sqrt(Lhd^2+Hdrive^2))/(rFw * Mu); %motor torsion spring torque
+Fspring =  Tspring /(sqrt(Lhd^2+Hdrive^2)); %force of spring acting on friction wheel
 Fnfric =  -Fspring; % normal force of frction wheel equal to spring for
-Fw = Tw/rFw;
+Fw = Tw/rFw; %driving force of motor 
 
-% position with reference to center of hinge axle 
-motorForces = [
-    0 Lhs+Lsw Hsw Fw 0 Fnfric 0 0 0;     %friction owheel contact point forces
-  %  -0.5*Ls Lhs 0 0 0 0.5*Fspring 0 0 0;  %spring forces acting on screw 1
-  %   0.5*Ls Lhs 0 0 0 0.5*Fspring 0 0 0;  %spring forces acting on screw 2 
-  %  0 0 0 0 0 0 Tw 0 0;
-    ];
+%friction wheel contact point force, forces are acting in x,y',z' must be
+%rotated to y and z
+motorForces = [0 Lhd Hdrive Fw 0 Fnfric 0 0 0;];
+    
+%rotates forces acting on friction wheel into coordinate system 
+    motorForces(1,5) = ((sqrt(2)/2)*(motorForces(1,5)+motorForces(1,6)));
+    motorForces(1,6) = ((sqrt(2)/2)*(motorForces(1,5)+motorForces(1,6)));
 
- hingeReactions = [ %-0.5*Ls La 0 1 1 1 0 1 0; 
-                   %0.5*Ls Lb 0 1 0 1 0 0 0;];
-                    0 0 0 1 1 1 1 1 1;];
+%reaction forces/moments acting at torsion spring hinge
+hingeReactions = [0 0 0 1 1 1 1 1 1;];
 
- hingeForce = -forceSolver(motorForces, hingeReactions)
+%forces acting on hinge therefor negative
+hingeForce = -forceSolver(motorForces, hingeReactions)
 
- %rotating hinge force to gondola coordinate system
+%rotating hinge force to gondola coordinate system
     hingeForce(1,4) = hingeForce(1,4);
     hingeForce(1,5) = ((sqrt(2)/2)*(hingeForce(1,5)+hingeForce(1,6)));
     hingeForce(1,6) = ((sqrt(2)/2)*(hingeForce(1,5)+hingeForce(1,6)));
     hingeForce(1,7) = hingeForce(1,7);
     hingeForce(1,8) = ((sqrt(2)/2)*(hingeForce(1,8)+hingeForce(1,9)));
     hingeForce(1,9) = ((sqrt(2)/2)*(hingeForce(1,8)+hingeForce(1,9)));
-    
 
-gondScrewReactionsWorst = [ -0.5*Ls La 0 1 1 1 2 2 0; 
-                        0.5*Ls Lb 0 0 1 0 2 2 0;];
+%worst reaction forces of hinge/gondola screws, almost all reaction from screw a    
+gondScrewReactionsWorst = [ -Ls La 0 1 1 1 2 3 0; 
+                             Ls Lb 0 0 1 0 2 3 0;];
 
 gondScrewReactionsWorstSolved = forceSolver(hingeForce, gondScrewReactionsWorst)
 
@@ -62,12 +80,58 @@ Fbolt = sqrt(gondScrewReactionsWorstSolved(1,4)^2+gondScrewReactionsWorstSolved(
 Ncompressive = Scompressive*10^6/ (Fbolt/(pi*(0.5*(Dwashero-Dwasheri))^2));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Calculating Gondola arm forces 
+%Calculating Gondola arm forces and acceration
 %assumptions: 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+pitchAngle = 0;     %pitch of airship [rads]
+thrustAngle = 0;    %angle of thrusters for acceration, 0 is straight forward [rads]
+gondAngle = 0;      %angle between gondolas [rads]
+m1 = 0.1;           %mass of gondola 1 in kg
+m2 = 0.1;           %mass of gondola 2 in kg
+aThrust = 1;        %magnitude of acceleration due to thrust [m/s^2]
 
+%%%%%%%%%%%%%% Forces %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+gondMotorForces = [-Ldrivex -Ldrivey Hdrive Fw 0 Fnfric 0 0 0; %acting on gond 1
+                   Ldrivex Ldrivey Hdrive Fw 0 -Fnfric 0 0 0;];%acting on gond 2
+               
+%%%%rotating motorForces to account for coordinate system change and gondAngle     
+    gondMotorForces(1,5) = ((sqrt(2)/2)*(gondMotorForces(1,5)+gondMotorForces(1,6)));
+    gondMotorForces(1,6) = ((sqrt(2)/2)*(gondMotorForces(1,5)+gondMotorForces(1,6)));
+    gondMotorForces(2,5) = ((sqrt(2)/2)*(gondMotorForces(2,5)+gondMotorForces(2,6)));
+    gondMotorForces(2,6) = ((sqrt(2)/2)*(gondMotorForces(2,5)+gondMotorForces(2,6)));
+    gondMotorForces(2,4) = cos(gondAngle)*gondMotorForces(2,4) + sin(gondAngle)*gondMotorForces(2,5);
+    gondMotorForces(2,5) = cos(gondAngle)*gondMotorForces(2,5) + sin(gondAngle)*gondMotorForces(2,4);
 
+weight = [Lcm1x Lcm1y Lcm1z 0 0 m1*g 0 0 0;      %acting on gond 1 
+          Lcm2x Lcm2y Lcm2z 0 0 m2*g 0 0 0;];    %acting on gond 2 
+     
+%%%%rotating weights to account for pitch angle
+    weight(1,4) = cos(pitchAngle)*weight(1,4) + sin(pitchAngle)*weight(1,6);
+    weight(2,6) = cos(pitchAngle)*weight(2,4) + sin(pitchAngle)*weight(2,6);
+
+accelerationForce = [Lcm1x Lcm1y Lcm1z m1*g 0 0 0 0 0;    %acting on gond 1 
+                     Lcm2x Lcm2y Lcm2z m2*g 0 0 0 0 0;];  %acting on gond 2
+               
+%%%%rotating accelerationForce to account for thrust angle
+    accelerationForce(1,4) = cos(thrustAngle)*accelerationForce(1,4) + sin(thrustAngle)*accelerationForce(1,6);
+    accelerationForce(2,6) = cos(thrustAngle)*accelerationForce(2,4) + sin(thrustAngle)*accelerationForce(2,6);
+
+gondForces =  [gondMotorForces ;  weight ; accelerationForce]        
+
+%%%%rotation for gondola angle changing postion of gond 2 forces 
+for i = 1:6
+    if mod(i,2) == 0
+    gondForces(i,1) = cos(gondAngle)*gondForces(i,1) + sin(gondAngle)*gondForces(i,3);
+    gondForces(i,3) = cos(gondAngle)*gondForces(i,3) + sin(gondAngle)*gondForces(i,1);
+    end
+end
+
+gondForces
+
+%%%%%%%%%%%%%% reactions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%gondReactions = [
+    
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Calculating Gondola acceleration 
